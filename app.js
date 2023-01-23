686788baadbcba3d629e58259cad39d67b19b8f6
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-23 17:37:16 -0600

    Tabla de multiplicar del 5, hecha unicamente por un ciclo FOR

---
+++ app.js
@@ -0,0 +1,12 @@
+//tabla del 5
+//limpiar la consola antes de ejecutar codigo
+console.clear();
+console.log('=================================================');
+console.log('=============        TABLA DEL 5      ===========');
+console.log('=================================================');
+
+const cinco = 5;
+
+for( let i = 1; i <= 10; i++ ) {
+    console.log(`${cinco} x ${i} = ${cinco * i }`);
+}
