b3f25d48ba3aab2438d232681007c32652742027
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-23 18:15:38 -0600

    Grabar la tabla en un archivo de texto con WRITEFILE y fs

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//file system = fs
+//se importa el pauqte 'fs
+const fs = require('fs');
+
 //tabla del 5
 //limpiar la consola antes de ejecutar codigo
 console.clear();
@@ -5,8 +9,23 @@ console.log('=================================================');
 console.log('=============        TABLA DEL 5      ===========');
 console.log('=================================================');
 
-const cinco = 5;
+const base = 2;
+//variable que se usara para grabar la tabla en un archivo
+let salida = '';
+
 
 for( let i = 1; i <= 10; i++ ) {
-    console.log(`${cinco} x ${i} = ${cinco * i }`);
+    //imprimir la tabla
+    //salida += sera usado para guradar la tabla en un archivo
+    salida += (`${base} x ${i} = ${base * i }\n`);
 }
+//imprimir en pantalla la tabla
+console.log( salida );
+
+// grabar en un archivo
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+
+    console.log(`Tabla-${base}.txt creado`);
+});
+
