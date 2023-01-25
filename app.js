92f3ab424d805543bf87daa2adc4f15e90478467
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-24 19:05:39 -0600

    Creacion de tablas con base desde consola process.argv(Forma no recomendada)

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ const { crearArchivo } = require('./helpers/multiplicar');
 //limpiar la consola antes de ejecutar codigo
 console.clear();
 
-const base = 9;
 
-//llamar funcion
+//obtener los arguemntos del process.argv
+//desestructuracion
+//podemos poner por defecto el valor del arg3
+const [ , , arg3 = 'base=5' ] = process.argv;
+//desestructuramos a partir de arg3.split()
+//estos es lo que vamos a separar  --base=5
+const [ , base = 5 ] = arg3.split('=');
+
+
+console.log( base );
+
+
+//const base = 9;
+
+// llamar funcion para crear archivo
 crearArchivo( base )
-    .then( respuesta => console.log( respuesta ) )
+    .then( nombreArchivo => console.log( nombreArchivo, 'creado' ) )
         .catch( err => console.log( err ) );
